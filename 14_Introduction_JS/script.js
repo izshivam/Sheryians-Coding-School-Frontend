8c0163902a44e565a290cc78e1c4c294127db0eb
izshivam <shivamkumarchoudhary8116@gmail.com>
2026-03-04 21:15:12 +0530

    Introduction JS

---
+++ 14_Introduction_JS/script.js
@@ -0,0 +1,161 @@
+// console.log('Hello JS');
+// console.log(10);
+
+// console.warn('Ths is warning');
+// console.error('This is error');
+
+// var a 
+// a = 10;
+// console.log(a);
+// console.log(sarthak);
+
+// var a = 10;
+// var b = 20;
+// console.log(a);
+// console.log(b);
+// console.log('a');
+// console.log(a,b);
+
+// var a = 10;
+// console.log(a);
+// a = 20;
+// console.log(a);
+
+
+// var a = 'Shivam'
+// var b = 20;
+// console.log(a);
+// console.log(b);
+// console.log(a,b);
+
+// var dulha = 'Harsh';
+// var dulha2 = 'Sarthak'
+// console.log(dulha, dulha2);
+
+
+/******** Arithmetic Operators ********/
+// var a = 10;
+// var b = 20;
+// var c = a+b;
+// console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(a,b);
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+
+/******  ******/
+// var groom = 'dulha'
+// var bride = 'dulhan'
+// console.log(groom, 'weds', bride);
+
+// var a = 10;
+// console.log('value of a is', a);
+
+/***********************/
+// alert('Are you sure');
+// confirm('Are u 18+')
+// prompt('Enter your age');
+
+
+// var ans = confirm('Are u 18+');
+// console.log(ans);
+
+// var username = prompt('Enter your age');
+// console.log(username);
+
+// var user = prompt('Enter your name');
+// var age = prompt('Enter your age');
+// console.log(user,age);
+
+/******** Increment & Decrement Operetors********/
+// var a = 10;
+// a++;
+// console.log(a);
+
+// var a = 10;
+// a--;
+// console.log(a);
+
+
+/******** let const var ********/
+// let a = 10;
+// console.log(a);
+
+// var a = 15;
+// console.log(a);
+
+// const a = 20;
+// console.log(a);
+
+/******** ********/
+// var a = 10;
+// console.log(a);
+// console.log(window);
+
+
+// const a = 10;
+// console.log(a);
+
+// const a = 10;
+// a = 15;
+// console.log(a);
+
+// var a = 10;
+// a = 20;
+// console.log(a);
+
+// let a = 10;
+// a = 15;
+// console.log(a);
+
+
+/********  ********/
+// var dulha = 'groom';
+// var dulhan = 'bride';
+// dulha = 'groom2'
+
+// console.log(dulha, 'weds', dulhan);
+
+
+// const dulha = 'groom';
+// const dulhan = 'bride';
+// dulha = 'groom2'
+
+// console.log(dulha, 'weds', dulhan);
+
+
+/********Diff. btw ler & var  ********/
+// var a = 10;
+// console.log(window);
+
+// let a = 10;
+// console.log(window);
+
+
+// if (10>5) {
+//     let a = 30;
+// }
+// console.log(a);
+
+
+// if (10>5) {
+//     let a = 30;
+//     console.log(a);
+// }
+
+
+// if (10>5) {
+//     var a = 10;
+//     console.log(a);
+// }
+
+
+// if (10>5) {
+//     var a = 10;
+// }
+// console.log(a);
