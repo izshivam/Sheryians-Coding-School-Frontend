f98230b3a723121f16574fe09cc80b435107da20
izshivam <shivamkumarchoudhary8116@gmail.com>
2026-03-10 21:25:19 +0530

    Basics of Arrays & Objects

---
+++ 20_Arrays&Objects_js/script.js
@@ -0,0 +1,75 @@
+/******
+what is arrays
+why arrays
+who uses array
+when to use array
+where to use array
+how to use array
+    how to create array []
+    how access memb of array 
+******/
+
+// var arr = [1,2,3,4,5];
+// arr[2];
+// arr[5];
+
+
+// var arr = [];
+
+
+// var arr2 = new Array();
+// arr2[0] = 15;
+// arr2[1] = 25;
+// arr2[2] = 20;
+
+
+// var arr = [];
+// var arr5 = [1,2,3,4,5]
+// var arr100 = new Array(100);
+
+
+// var arr = new Array(30);
+// for(var i = 0; i<=30; i++ ) {
+//     arr[i] = i;
+// };
+
+
+// var arr = [5, "Shivam", [], true, {}, function(){}, 2.4, NaN, undefined];
+
+
+/************ OBJECTS ************/
+/******** 
+what is object
+why object
+when to make an object
+how to create an object
+how to access an object
+********/
+
+// var obj = {
+//     key:"value",
+//     key:"value",
+//     key:"value",
+//     key:"value"
+// };
+
+
+// var obj = {
+//     name : "Shivam",
+//     age : 27,
+//     email : "ok@gmail.com",
+//     isAvailable : true
+// };
+
+
+// var obj = {}
+// var obj2 = new Object();
+
+
+// var obj = {
+//     name : "Shivam",
+//     age : 27,
+//     email : "ok@gmail.com",
+//     isMarried : false
+// };
+// obj.email;
