d9c608e519c4c4c73f6f135885d50c3598eafe63
izshivam <shivamkumarchoudhary8116@gmail.com>
2026-03-12 20:20:16 +0530

    map, forEach, filter, array 5 & loop 5 questions

---
+++ 22_JS_Practice2/script.js
@@ -0,0 +1,108 @@
+/** Map **/
+// var arr = [1,2,3,4,5];
+// var ans = arr.map(function(value){
+//     return 12;
+// });
+
+
+/** For Each **/
+// var arr = [1,2,3,4,5];
+// arr.forEach(function(value){
+//     console.log(value)
+// });
+
+
+/** Filter **/
+// var arr = [1,2,3,4,5];
+// var ans = arr.filter(function(value){
+//     return true;
+// });
+
+
+// var arr = [1,2,3,4,5];
+// var ans = arr.filter(function(value){
+//     return false;
+// });
+
+
+// var arr = [1,2,3,4,5];
+// var ans = arr.filter(function(value){
+//     return value>2;
+// });
+
+
+// var arr = [1,2,3,4,5];
+// var ans = arr.filter(function(value){
+//     return value === 3;
+// });
+
+
+/** Reduce **/
+// var arr = [1,2,3,4,5];
+// arr.reduce(function(accumulator, key){
+//     console.log(key)
+// }, 0);
+
+
+// var arr = [1,2,3,4,5];
+// var ans = arr.reduce(function(accumulator, key){
+//     return accumulator + key;
+// }, 0);
+
+
+// var arr = [1,2,3,4,5];
+// var ans = arr.reduce(function(accumulator, key){
+//     return accumulator * key;
+// }, 1);
+
+
+// 9. Create an array of 5 no and log the sum using .reduce()
+// var arr = [1,2,3,4,5];
+// var ans = arr.reduce(function(acc, value){
+//     return acc + value;
+// }, 0);
+
+
+/************* Loop (5 Questions) ****************/
+// 11.Write a for loop to print numbers from 10 to 1 in reverse.
+// var i = 10;
+// while(i>0){
+//     console.log(i);
+//     i--;
+// };
+
+
+// 12.Use a while loop to print multiples of 3 from 3 to 30.
+// var i = 3;
+// while(i<31){
+//     console.log(i);
+//     i+=3;
+// };
+
+
+// 13.Write a program to calculate the sum of numbers from 1 to 100 using a loop.
+// var sum = 0;
+// for(i = 1; i<101; i++){
+//     sum = sum + i;
+// };
+// console.log(sum);
+
+
+// 14.Create a nested loop to print a star pattern.
+// 15.Use a for loop to iterate over the string "JavaScript".
+// var str = "javascript";
+// for(var i of str){
+//     console.log(i);   
+// };
+
+/************* Array (5 Questions) ****************/
+// 16.Remove duplicate values from an array.
+// var arr = [1,1,1,2,2,2,2,2,3,5,4,8,8,8,8,6,6,6];
+// var arr2 = [...new Set(arr)];
+
+
+// 17.Find the second largest number in an array.
+
+// 18.Sort an array in descending order.
+// 19.Reverse an array without using .reverse()
+// 20.Find the most frequent element in an array()
